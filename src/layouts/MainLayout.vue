96fb693e7c3eca4476b87eabe572dd313d70800f
Sadra Saderi <sadrassx@gmail.com>
2022-07-19 19:04:41 +0430

    Update MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,22 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-page-sticky
-      position="top-left"
-      :offset="[18, 18]"
-      style="z-index: 1000"
-      v-if="testMethod()"
-    >
-      <q-btn
-        fab
-        flat
-        dense
-        round
-        icon="menu"
-        aria-label="Menu"
-        @click="leftDrawerOpen = !leftDrawerOpen"
-      />
-    </q-page-sticky>
-
     <q-drawer behavior="desktop" show-if-above content-class="bg-grey-4">
       <q-list>
         <q-item-label header class="label text-grey-10 q-py-md-xl text-center">
@@ -60,28 +43,6 @@ const linksData = [
   components: { EssentialLink },
 })
 export default class MainLayout extends Vue {
-  showFab = true;
-  leftDrawerOpen = false;
   essentialLinks = linksData;
-  // this.showFab = window.innerWidth < 1005;
-  // return window.innerWidth < 1005;
-
-  testMethod() {
-    return window.innerWidth < 1005;
-  }
-
-  // resizeHandler(event: Event) {
-  //   console.log(event);
-  // }
-
-  // created() {
-  //   window.addEventListener('resize', this.resizeHandler).bind(this);
-  // }
-
-  // destroyed() {
-  //   window.removeEventListener('resize', (event) => {
-  //     this.resizeHandler(event);
-  //   });
-  // }
 }
 </script>

7932c35d00f563e1e1506ca459bbaaba62b4b8f5
Sadra Saderi <sadrassx@gmail.com>
2022-07-27 21:47:33 +0430

    Update Service.vue

--- src/pages/Service.vue
+++ src/pages/Service.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="page-content q-py-md q-px-md q-px-md-lg q-px-lg-xl">
-    <h4>Marketing</h4>
+    <h4>{{ service?.title ?? 'Service' }}</h4>
 
     <div class="row">
-      <div class="col-4 q-pa-sm" v-for="item in data" :key="item.id">
+      <div class="col-4 q-pa-sm" v-for="item in plugins" :key="item.id">
         <PluginCard
           :title="item.title"
           :details="item.details"
@@ -17,38 +17,32 @@
 <script lang="ts">
 import { Vue, Component, Watch } from 'vue-property-decorator';
 
-// import AxiosClient from 'src/services/axios-client';
-import FetchClient from 'src/services/fetch-client';
 import PluginCard from 'components/PluginCard.vue';
-import { ClientInterface } from 'src/services/interfaces';
-
-type dataType = {
-  id: string;
-  title: string;
-  details: string;
-  status: string;
-};
-
-// let client: ClientInterface = new AxiosClient();
-let client: ClientInterface = new FetchClient();
+import { PluginInterface, ServiceInterface } from 'src/store/data/state';
+import {
+  clearPlugins,
+  getPlugins,
+  getServices,
+  storePlugins,
+} from 'src/services/store-data';
 
 @Component({
   components: { PluginCard },
 })
 export default class Service extends Vue {
   slug: string = this.$route.params.slug;
-  data: Array<dataType> = [];
+
+  get service(): ServiceInterface | undefined {
+    return getServices(this.$store).find((item) => item.slug == this.slug);
+  }
+
+  get plugins(): PluginInterface[] {
+    return getPlugins(this.$store);
+  }
 
   updateData() {
-    client
-      .get(`api/services/${this.slug}/plugins`)
-      .then((response) => {
-        this.data = (response.data as { [key: string]: object })
-          .plugins as Array<dataType>;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    clearPlugins(this.$store);
+    storePlugins(this.$store, this.slug);
   }
 
   @Watch('$route', { immediate: true, deep: true })
